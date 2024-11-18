3727dfbf4054a84b652dbf0afa679310ccd501b4
muskuloes <vic.tuekam@gmail.com>
2024-11-18 22:31:42 +0100

    update treesitter config

--- .config/nvim/lua/muskuloes/plugins/treesitter.lua
+++ .config/nvim/lua/muskuloes/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return function()
   local ok, ts_configs = pcall(require, "nvim-treesitter.configs")
 
-  require('ts_context_commentstring').setup {} 
+  require("ts_context_commentstring").setup {}
   vim.g.skip_ts_context_commentstring_module = true
 
   if not ok then
@@ -9,13 +9,24 @@ return function()
   end
 
   ts_configs.setup {
-    ensure_installed = { "javascript", "json", "lua", "markdown", "markdown_inline", "python", "r", "rnoweb", "tmux", "typescript", "yaml" },
+    ensure_installed = {
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "r",
+      "rnoweb",
+      "tmux",
+      "typescript",
+      "yaml",
+    },
     sync_install = false,
     ingore_install = { "" },
     autopairs = {
       enable = true,
     },
-    autotag = { enable = true },
     highlight = { enable = true, disable = { "" }, additional_vim_regex_highlighting = true },
     indent = { enable = true, disable = { "yaml" } },
     context_commentstring = { enable = true },
